2f0729e737abf1bbad948fa20433af553f924b1a
big riz <66181913+big-riz@users.noreply.github.com>
2023-09-13 19:07:50 -0500

    hm

--- build/index.service.worker.js
+++ build/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1694649643|2339488777";
+const CACHE_VERSION = "1694650054|2750488905";
 const CACHE_PREFIX = "pepe-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
